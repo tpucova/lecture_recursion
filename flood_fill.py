3e66eb8f3ded02e89435f1a5e073d9dded0cf723
tpucova <271400@vutbr.cz>
2025-04-28 14:40:34 +0200

    'whaaaat'

--- flood_fill.py
+++ flood_fill.py
@@ -1,16 +1,31 @@
 import matplotlib.pyplot as plt
 
+def flood_fill(img, x, y):
+    if x >= img.shape[0] or y >= img.shape[1] or x < 0 or y < 0:
+        return img
+    elif img[x,y] == 0 or img[x,y] == 2:
+        return img
+    img[x,y] = 2
+    plt.imshow(img, cmap="gray")
+    plt.show(block=False)
+    plt.pause(0.05)
+    plt.clf()
+    img = flood_fill(img,x+1, y)
+    img = flood_fill(img, x-1, y)
+    img = flood_fill(img, x, y+1)
+    img = flood_fill(img, x, y-1)
+    return img
 
 def main():
-    img = plt.imread("files/img0.png")[:, :, 0]
-    # img = plt.imread("files/img1.png")[:, :, 0]
+    # img = plt.imread("files/img0.png")[:, :, 0]
+    img = plt.imread("files/img1.png")[:, :, 0]
     # img = plt.imread("files/img2.png")[:, :, 0]
 
-    # img = floodfill(img, 0, 0)
+    img = flood_fill(img, 0, 0)
 
     plt.imshow(img, cmap="gray")
     plt.show(block=False)
-    plt.pause(5)
+    plt.pause(1)
     plt.clf()
 
 
