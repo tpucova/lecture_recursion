6647edc52c8576c48ca7886ba976d1c5f99245e7
tpucova <271400@vutbr.cz>
2025-04-28 14:06:52 +0200

    'cosi'

--- fibo.py
+++ fibo.py
@@ -1,7 +1,18 @@
 
+def recursive_nth_fibo(n):
+   if n == 0:
+       return 0
+   elif n == 1:
+       return 1
+   else:
+       fibo_1 = recursive_nth_fibo(n-1)
+       fibo_2 = recursive_nth_fibo(n-2)
+       return fibo_1 + fibo_2
 def main():
     pass
 
 
 if __name__ == "__main__":
-    main()
+    my_n = 4
+    nth_fibo = recursive_nth_fibo(my_n)
+    print(nth_fibo)
